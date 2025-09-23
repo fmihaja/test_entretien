10548c3379c81380bfe0b4133cc145cfeb3ece4b
fmihaja <fmihaja@gmail.com>
2025-09-23 19:21:19 +0200

    ajout du logo fait

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,12 @@ function App() {
                     <div className="absolute -z-1 inset-0 bg-gradient-to-r from-black/90 to-transparent"></div>
                     <div className="h-screen w-1/15 bg-[#4097FF]/35"></div>
                     <div className="relative h-screen w-full pt-10 flex flex-col justify-between items-center">
-                        <div className="w-[80%] h-30 bg-amber-300"></div>
+                        <div className="w-[80%] h-30 border-b-2 border-b-white flex justify-between items-center">
+                            <div className="w-1/2 h-full">
+                                <img src="/src/assets/home/logo_azur.png" alt="" srcset="" className="w-[23%]" />
+                            </div>
+                            <div className="w-1/2 h-full bg-red-300"></div>
+                        </div>
                     </div>
                 </div>
             </div>
