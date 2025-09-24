99a267af553ec498cd960f4cb87e82d02c1c0666
fmihaja <fmihaja@gmail.com>
2025-09-24 10:04:47 +0200

    debut de refactorisation du code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import Activity from "./pages/Activity";
+import Comfort from "./pages/Comfort";
 import Discovery from "./pages/Discovery";
+import GMap from "./pages/GMap";
+import Home from "./pages/Home";
+import Location from "./pages/Location";
 
 function App() {
     return (
         <>
+            <Home/>
+            <Comfort />
+            <Location />
+            <GMap />
+            <Discovery />
             <Activity />
         </>
     );
